fa2881951ee9b5546a175a76690b5c4d091d88d5
Prabhjeet <Prabhjeet.singhjii@gmail.com>
2024-11-19 13:27:12 +0530

    middleof linkedList

--- src/linkedlist/LinkedListLength.java
+++ src/linkedlist/LinkedListLength.java
@@ -1,23 +1,11 @@
 package linkedlist;
 
-// Definition for singly-linked list node
- class Node {
-    int data;
-    Nodee next;
-
-    Node(int val) {
-        this.data = val;
-        this.next = null;
-    }
-}
 
 public class LinkedListLength {
 
-    // Function to find the length of the linked list
-    public int findLength(Nodee head) {
+    public int findLength(Node head) {
         int length = 0;
 
-        // Traverse the list using head directly
         while (head != null) {
             length++;
             head = head.next;
@@ -27,14 +15,12 @@ public class LinkedListLength {
     }
 
     public static void main(String[] args) {
-        // Create a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
-        Nodee head = new Nodee(1);
-        head.next = new Nodee(2);
-        head.next.next = new Nodee(3);
-        head.next.next.next = new Nodee(4);
-        head.next.next.next.next = new Nodee(5);
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
 
-        // Calculate and print the length of the linked list
         LinkedListLength solution = new LinkedListLength();
         int length = solution.findLength(head);
         System.out.println("Length of the linked list: " + length);
