9d61e4207d77b3ce5d101085fb629e6458d9ad19
Prabhjeet <prabhjeetsinghjii@gmail.com>
2025-02-01 22:49:12 +0530

    Leetcode class

---
+++ src/array/SingleNumber136.java
@@ -0,0 +1,20 @@
+package array;
+
+import java.util.Arrays;
+
+public class SingleNumber136 {
+
+    public int singleNumber(int[] nums) {
+        int n= nums.length;
+
+        Arrays.sort(nums);
+
+        for(int i=0;i<n-1;i=i+2){
+            if(nums[i]!=nums[i+1]){
+                return nums[i];
+            }
+
+        }
+        return nums[n-1];
+    }
+}
